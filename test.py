3f115344f6f5aeb83ab1cefed04edf30cf1bee79
pfilbin90 <pjf719@gmail.com>
2019-02-06 15:41:41 -0600

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-import requests
-offset = 432530405
-
-method = requests.get('https://api.telegram.org/bot755600779:AAEeoNRbvz0g21cKielyp_HueK09wyaaxuQ/getUpdates?timeout=100&offset={}'.format(str(offset + 1)))
-print(str(method.status_code))
