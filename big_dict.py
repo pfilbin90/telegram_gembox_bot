5bc3817fe27d4ed3d5e865a9bcc180a2a64085c6
pfilbin90 <pjf719@gmail.com>
2019-02-19 23:13:03 -0600

    more updates; including readme

--- big_dict.py
+++ big_dict.py
@@ -1,37 +1,42 @@
 the_big_dict = { 
-    '/welcome@GemboxMiboss_bot' : """Thanks for choosing Gembox_bot.  For a list of commands, type /commands and press Enter""",
-    
-    
+    '/welcome@GemboxMiboss_bot' : """Thanks for choosing Gembox_bot.  For a list of commands, type /commands and press Enter""",  
 }
 
+"""             
+            deletestr = '@GemboxMiboss_bot delete'
+            ifsofacto = 'ifsofacto'
+            ifso_space = 'ifso facto'
+            ifso = 'ifso'
+            ifspace = 'if so'
+"""
 hello_dict = {
-    '@GemboxMiboss_bot' : 'Hello there {}'.format(user),
-    'hi @GemboxMiboss_bot' : 'Hi {}!'.format(user),
-    'hiya @GemboxMiboss_bot': 'hiya {}'.format(user),
-    'hey @GemboxMiboss_bot' : 'Hey {}'.format(user),
-    'hello @GemboxMiboss_bot' : 'Hello {}'.format(user),
-    'howdy @GemboxMiboss_bot' : 'Howdy {}'.format(user),
-    'greetings @GemboxMiboss_bot' : 'Grettings to you as well {}'.format(user),
-    'bonjour @GemboxMiboss_bot' : 'Bonjour {}'.format(user),
-    'buenos dias @GemboxMiboss_bot' : 'Si, buenos dias {}'.format(user),
-    'good day @GemboxMiboss_bot' : 'Good day {}'.format(user),
-    'good morning @GemboxMiboss_bot' : 'good morning {}'.format(user),
-    'how are you @GemboxMiboss_bot' : 'I am fine, thanks for asking {}'.format(user),
+    '@GemboxMiboss_bot' : 'Hello there {}',
+    'hi @GemboxMiboss_bot' : 'Hi {}!',
+    'hiya @GemboxMiboss_bot': 'hiya {}',
+    'hey @GemboxMiboss_bot' : 'Hey {}',
+    'hello @GemboxMiboss_bot' : 'Hello {}',
+    'howdy @GemboxMiboss_bot' : 'Howdy {}',
+    'greetings @GemboxMiboss_bot' : 'Grettings to you as well {}',
+    'bonjour @GemboxMiboss_bot' : 'Bonjour {}',
+    'buenos dias @GemboxMiboss_bot' : 'Si, buenos dias {}',
+    'good day @GemboxMiboss_bot' : 'Good day {}',
+    'good morning @GemboxMiboss_bot' : 'good morning {}',
+    'how are you @GemboxMiboss_bot' : 'I am fine, thanks for asking {}',
     'how goes it @GemboxMiboss_bot' : 'It goes',
     "what's happening @GemboxMiboss_bot" : 'I am living a pretty dull life tbh',
     "whats happening @GemboxMiboss_bot" : 'I am living a pretty dull life tbh',
     "what's up @GemboxMiboss_bot" : 'Well, Trump is President right now unfortunately',
-    '@GemboxMiboss_bot hi' : 'Hi {}!'.format(user),
-    '@GemboxMiboss_bot hiya': 'hiya {}'.format(user),
-    '@GemboxMiboss_bot hey' : 'Hey {}'.format(user),
-    '@GemboxMiboss_bot hello' : 'Hello {}'.format(user),
-    'howdy @GemboxMiboss_bot' : 'Howdy {}'.format(user),
-    '@GemboxMiboss_bot greetings' : 'Greetings to you as well {}'.format(user),
-    '@GemboxMiboss_bot bonjour' : 'Bonjour {}'.format(user),
-    '@GemboxMiboss_bot buenos dias' : 'Si, buenos dias {}'.format(user),
-    '@GemboxMiboss_bot good day ' : 'Good day {}'.format(user),
-    '@GemboxMiboss_bot good morning' : 'good morning {}'.format(user),
-    '@GemboxMiboss_bot how are you' : 'I am fine, thanks for asking {}'.format(user),
+    '@GemboxMiboss_bot hi' : 'Hi {}!',
+    '@GemboxMiboss_bot hiya': 'hiya {}',
+    '@GemboxMiboss_bot hey' : 'Hey {}',
+    '@GemboxMiboss_bot hello' : 'Hello {}',
+    'howdy @GemboxMiboss_bot' : 'Howdy {}',
+    '@GemboxMiboss_bot greetings' : 'Greetings to you as well {}',
+    '@GemboxMiboss_bot bonjour' : 'Bonjour {}',
+    '@GemboxMiboss_bot buenos dias' : 'Si, buenos dias {}',
+    '@GemboxMiboss_bot good day ' : 'Good day {}',
+    '@GemboxMiboss_bot good morning' : 'good morning {}',
+    '@GemboxMiboss_bot how are you' : 'I am fine, thanks for asking {}',
     '@GemboxMiboss_bot how goes it' : 'It goes',
     "@GemboxMiboss_bot what's happening " : 'I am living a pretty dull life tbh',
     "@GemboxMiboss_bot whats happening" : 'I am living a pretty dull life tbh',
--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import update_gembox
 import datetime
 import re
 from dbhelper import DBHelper
+from big_dict import hello_dict
+
 bot = telegram_chatbot("config.cfg")
 insult = insultGenerator()
 
@@ -30,62 +32,22 @@ while True:
     if updates:
         for item in updates:
             update_id = item["update_id"]
-            user = item["message"]["from"]["first_name"] 
+            try:    
+                user = item["message"]["from"]["first_name"]
+            except:
+                continue
+            try: 
+                message = str(item["message"]["text"])
+            except:
+                message = str(item['edited_message']['text'])
             epoch = item["message"]["date"]
             date = datetime.datetime.fromtimestamp(epoch).strftime('%m-%d-%Y')
             gemresponse = "{}'s quote has been added to the Gembox Vault".format(user)
             chat_id = item["message"]["chat"]["id"]
-            from_ = item["message"]["from"]["id"]
             try:
-                message = str(item["message"]["text"])
+                quote_user= item["message"]["reply_to_message"]["from"]["first_name"]
             except:
-                message = str(item["edited_message"]["text"])
-            deletestr = '@GemboxMiboss_bot delete'
-            ifsofacto = 'ifsofacto'
-            ifso_space = 'ifso facto'
-            ifso = 'ifso'
-            ifspace = 'if so'
-            '''
-            response = thing[message]
-            If response:
-            send(response)
-            '''
-            if message == '/welcome@GemboxMiboss_bot':
-                bot.send_message("""Thanks for choosing Gembox_bot.  For a list of commands, type /commands and press Enter""", chat_id)
-            elif message == '@GemboxMiboss_bot view' or message == '@GemboxMiboss_bot /view':
-                try:
-                    reply = read_only(message)
-                    bot.send_message(reply, chat_id)
-                except:
-                    message = None
-            elif message == '@GemboxMiboss_bot' or message == '@GemboxMiboss_bot hi' or message == '@GemboxMiboss_bot hello' or message == '@GemboxMiboss_bot yo' or message == 'hi @GemboxMiboss_bot' or message == 'Hi @GemboxMiboss_bot':
-                bot.send_message(("Hello there {}").format(user), chat_id)
-            elif message == '@GemboxMiboss_bot fuck you':
-                bot.send_message("Fuck you too, scrub", chat_id)
-            elif message == '@GemboxMiboss_bot what\'s my name?' or message == '@GemboxMiboss_bot what\'s my name':
-                bot.send_message(("Usually your name is {}").format(user), chat_id)
-            elif message == '@GemboxMiboss_bot gembox' or message == 'gembox' or message == 'Gembox' or message == '/gembox' or message == '/Gembox':
-                try:
-                    try:
-                        msg = item["message"]["reply_to_message"]["text"]
-                        add_and_reply(msg, user, date, gemresponse, chat_id, from_)
-                    except:
-                        bot.send_message("You must QUOTE or REPLY a message to add it to the Gembox Vault", chat_id)
-                except Exception as e: bot.send_message(e, chat_id)
-            elif deletestr in message:
-                num = message[24:]
-                update_gembox.gembox_delete(num, chat_id)
-                reply = update_gembox.gembox_view()
-                bot.send_message(reply, chat_id)
-            elif message == '/vivaldi':
-                reply = 'https://goo.gl/jUJ7R1'
-                bot.send_message(reply, chat_id)
-            elif message == '/commands@GemboxMiboss_bot' or message == '@GemboxMiboss_bot commands':
-                update_gembox.gembox_commands(chat_id)
-            elif re.findall(r"\b" + ifso + r"\b", message) or re.findall(r"\b" + ifsofacto + r"\b", message) or re.findall(r"\b" + ifso_space + r"\b", message) or re.findall(r"\b" + ifspace + r"\b", message): 
-                bot.send_message("You spelled 'ipso' wrong", chat_id)
-                bot.send_message('https://en.wikipedia.org/wiki/Ipso_facto', chat_id)
-            elif '@GemboxMiboss_bot' in message:
-                replyinsult = insult.getInsult()
-                bot.send_message("There was no command request, so here's a random insult", chat_id)
-                bot.send_message(replyinsult, chat_id)
+                continue
+            for key in hello_dict:
+                if message.lower() == key.lower():
+                    print(hello_dict[key])
